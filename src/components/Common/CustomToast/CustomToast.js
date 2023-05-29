a40e1b911789be34810d14dd2921ee7b534a7fe0
Sandesh Hyoju <sundesz@gmail.com>
2023-05-29 13:04:52 +0300

    Change toast position to center

--- src/components/Common/CustomToast/CustomToast.js
+++ src/components/Common/CustomToast/CustomToast.js
@@ -4,13 +4,14 @@ import { toast } from "react-toastify";
 const CustomToast = (
   message = "Success",
   status = "success",
-
+  position = "top-center",
   theme = "dark",
   autoClose = 5000,
   closeOnClick = true,
   draggable = true
 ) => {
   return toast[status](message, {
+    position,
     autoClose,
     closeOnClick,
     draggable,
