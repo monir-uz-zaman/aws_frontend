0b85b428784393a0ddd0710f11129356313b23da
Sandesh Hyoju <sundesz@gmail.com>
2023-06-01 11:45:47 +0300

    Remove position option from toast

--- src/components/Common/CustomToast/CustomToast.js
+++ src/components/Common/CustomToast/CustomToast.js
@@ -4,14 +4,12 @@ import { toast } from "react-toastify";
 const CustomToast = (
   message = "Success",
   status = "success",
-  position = "top-center",
   theme = "dark",
   autoClose = 5000,
   closeOnClick = true,
   draggable = true
 ) => {
   return toast[status](message, {
-    position,
     autoClose,
     closeOnClick,
     draggable,
